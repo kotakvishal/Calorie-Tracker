dac20b2f68cf54ccd0dcec07595593f7741c3c04
kotakvishal <kotakvishal2001@gmail.com>
2024-06-06 22:38:29 +0530

    Add Welcome Screen

--- app/src/main/java/ktk/vishdroid/calorytracker/ui/theme/Theme.kt
+++ app/src/main/java/ktk/vishdroid/calorytracker/ui/theme/Theme.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import ktk.vishdroid.calorytrackerprep.ui.theme.Typography
+import ktk.vishdroid.core_ui.Dimensions
+import ktk.vishdroid.core_ui.LocalSpacing
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
@@ -34,16 +36,19 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CaloryTrackerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors, typography = Typography, shapes = Shapes, content = content
+        )
+    }
+
 }
