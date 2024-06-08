42fd1b5cb249e5fd34d0231a9a6f088f5417cf4b
kotakvishal <kotakvishal2001@gmail.com>
2024-06-08 15:36:14 +0530

    Impl Preferences Util

---
+++ app/src/main/java/ktk/vishdroid/calorytracker/di/AppModule.kt
@@ -0,0 +1,31 @@
+package ktk.vishdroid.calorytracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import ktk.vishdroid.core.domain.DefaultPreferences
+import ktk.vishdroid.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+}
